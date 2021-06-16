462500cc3ef5147ffe71b7e924a0e7bc7f63c5e5
Alessandro Cere <ale.cere@gmail.com>
2021-06-16 10:22:46 +0800

    Initial commit

---
+++ fns/lambda.py
@@ -0,0 +1,83 @@
+import json
+import os
+
+import boto3
+from aws_lambda_powertools import Logger
+from tenacity import retry, stop_after_attempt, wait_exponential
+
+logger = Logger()
+client = boto3.client("rekognition")
+collection_id = os.getenv("COLLECTION_NAME")
+max_faces_index = os.getenv("MAX_FACE_INDEX", 1)
+max_faces_match = os.getenv("MAX_FACE_MATCH", 100)
+threshold = os.getenv("FACE_MATCHING_THRESHOLD", 50)
+
+# @logger.inject_lambda_context(log_event=True)
+def handler(event, context):
+    try:
+        for record in event["Records"]:
+            payload = record["body"]
+            for r in json.loads(payload)["Records"]:
+                # Extract the reference of the image that triggered the lambda
+                bucket = r["s3"]["bucket"]["name"]
+                object_key = r["s3"]["object"]["key"]
+                logger.info(f"bucket:{bucket}, key:{object_key}")
+
+                # Extract semantinc info form the object key
+                image_id = object_key.split("/")[-1]
+                customer_id = image_id.rsplit("_", 1)[0]
+
+                # add image to collection and search for matches
+                matches = add_search_twin(
+                    bucket,
+                    object_key,
+                    image_id=image_id,
+                    collection_id=collection_id,
+                )
+                logger.info(f"Found {len(matches)} matches in the collection")
+                logger.info(matches)
+
+                # check if there's any match with other customers_id
+                res = [
+                    k
+                    for k in matches
+                    if k["Face"]["ExternalImageId"].rsplit("_", 1)[0] != customer_id
+                ]
+
+                logger.info(f"Matches that require manual investigation {res}")
+
+        return {"status": "success", "message": res}
+    except Exception:
+        logger.exception("Received an exception")
+
+
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=0.2, min=1, max=10))
+def search_faces_retry(**kwargs):
+    return client.search_faces(**kwargs)
+
+
+def add_search_twin(bucket, s3_path, image_id, collection_id):
+    response_add = client.index_faces(
+        CollectionId=collection_id,
+        Image={"S3Object": {"Bucket": bucket, "Name": s3_path}},
+        ExternalImageId=image_id,
+        MaxFaces=max_faces_index,
+        QualityFilter="AUTO",
+        DetectionAttributes=["DEFAULT"],
+    )
+    try:
+        face_id = response_add["FaceRecords"][0]["Face"]["FaceId"]
+    except:
+        print("no face detected in image")
+        return
+
+    response_search = search_faces_retry(
+        CollectionId=collection_id,
+        FaceId=face_id,
+        FaceMatchThreshold=threshold,
+        MaxFaces=max_faces_match,
+    )
+
+    faceMatches = response_search["FaceMatches"]
+    return faceMatches
+    # return {k["Face"]["ExternalImageId"]: k["Similarity"] for k in faceMatches}
--- rekognition_batch_detect/rekognition_batch_detect_stack.py
+++ rekognition_batch_detect/rekognition_batch_detect_stack.py
@@ -1,15 +1,105 @@
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_lambda as lambda_
+from aws_cdk import aws_lambda_event_sources as es
+from aws_cdk import aws_s3 as s3
+from aws_cdk import aws_s3_notifications as s3n
+from aws_cdk import aws_sqs as sqs
 from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import custom_resources as cr
+from aws_cdk.aws_lambda_python import PythonLayerVersion
 
 
 class RekognitionBatchDetectStack(cdk.Stack):
-
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        collection_id = cdk.CfnParameter(
+            self, "RekognitionCollectionId", default="TestCollection"
+        )
+
+        # Define the main SQS queue
+        queue = sqs.Queue(self, "Queue")
+
+        # Define the Bucket used to host the data
+        bk_test_images = s3.Bucket(self, "Bucket with Test Images")
+
+        # Send notification for object creation events from S3 to the queue
+        bk_test_images.add_event_notification(
+            event=s3.EventType.OBJECT_CREATED, dest=s3n.SqsDestination(queue)
+        )
+
+        # Create the Rekognition collection, and ignore if the collection already exists
+        aws_rekognition_cr = cr.AwsCustomResource(
+            self,
+            "CR:RekognitionCollection",
+            on_create={
+                "service": "Rekognition",
+                "action": "createCollection",
+                "parameters": {"CollectionId": collection_id.to_string()},
+                "physical_resource_id": cr.PhysicalResourceId.of(
+                    "RekognitionCollection"
+                ),
+                "ignore_error_codes_matching": "ResourceAlreadyExistsException",
+            },
+            on_delete={
+                "service": "Rekognition",
+                "action": "deleteCollection",
+                "parameters": {"CollectionId": collection_id.to_string()},
+            },
+            policy=cr.AwsCustomResourcePolicy.from_sdk_calls(
+                resources=cr.AwsCustomResourcePolicy.ANY_RESOURCE
+            ),
+        )
+
+        # Define the main lambda function, starting from the libraries layer
+        # (possible alternative is to define a containerized python function, but it's slower to test locally)
+        tenacity_lambda_layer = PythonLayerVersion(
+            self,
+            "TenacityLambdaLayer",
+            entry="fns/libs",
+            compatible_runtimes=[lambda_.Runtime.PYTHON_3_8],
+            description="Tenacity and AWS-Lambda-Powertools",
+            layer_version_name="1",
+        )
+
+        # The lambda function proper
+        process_fn = lambda_.Function(
+            self,
+            "ConsumerFn",
+            code=lambda_.Code.from_asset("fns"),
+            handler="lambda.handler",
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            environment={"COLLECTION_NAME": collection_id.to_string()},
+            layers=[
+                tenacity_lambda_layer,
+            ],
+            timeout=cdk.Duration.seconds(60),
+            reserved_concurrent_executions=5,
+        )
+
+        # Setup the trigger for the lambda function. CDK takes care of
+        # the relevant IAM policies in the lambda role
+        process_fn.add_event_source(es.SqsEventSource(queue))
+
+        # Add read-write privileges to the lambda function to access the bucket
+        bk_test_images.grant_read_write(process_fn)
+
+        # Grant necessary Rekognition privileges to lambda fn
+        process_fn.role.add_to_policy(
+            iam.PolicyStatement(
+                resources=[
+                    f"arn:aws:rekognition:{self.region}:{self.account}:collection/{collection_id.to_string()}"
+                ],
+                actions=[
+                    "rekognition:CompareFaces",
+                    "rekognition:DetectFaces",
+                    "rekognition:DetectLabels",
+                    "rekognition:IndexFaces",
+                    "rekognition:ListCollections",
+                    "rekognition:ListFaces",
+                    "rekognition:SearchFaces",
+                    "rekognition:SearchFacesByImage",
+                    "rekognition:ListTagsForResource",
+                ],
+            )
+        )
